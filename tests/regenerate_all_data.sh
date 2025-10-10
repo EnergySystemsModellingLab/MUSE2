6ee219593878ff10809ec05d27471d36e2137e55
Alex Dewar <a.dewar@imperial.ac.uk>
2025-10-10 15:05:23 +0100

    regenerate_all_data.sh: Use --overwrite flag cf. manually deleting test data

--- tests/regenerate_all_data.sh
+++ tests/regenerate_all_data.sh
@@ -3,9 +3,6 @@
 mydir=$(dirname "$0")
 cd "$mydir"
 
-echo Cleaning output folder
-rm -rf data/*
-
 echo Building MUSE2
 examples=$(cargo run example list 2> /dev/null)
 
@@ -13,9 +10,9 @@ for example in $examples; do
     echo Generating data for example: $example
 
     # We only need debug files for the simple model
-    unset extra_args
+    extra_args=--overwrite
     if [ $example = simple ]; then
-        extra_args=--debug-model
+        extra_args+=" --debug-model"
     fi
 
     env MUSE2_LOG_LEVEL=off cargo run example run $extra_args -o "data/$example" "$example" 2> /dev/null
