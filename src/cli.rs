6b68ae2b1b420c0cb10c1d2e1c5c7f44f24d078b
Alex Dewar <a.dewar@imperial.ac.uk>
2025-10-13 12:20:15 +0100

    Fix rendering of help when no command provided

--- src/cli.rs
+++ src/cli.rs
@@ -115,14 +115,14 @@ pub fn run_cli() -> Result<()> {
         return Ok(());
     }
 
-    let Some(command) = cli.command else {
-        // Output program help in markdown format
-        let help_str = Cli::command().render_long_help().to_string();
-        println!("{help_str}");
-        return Ok(());
-    };
+    if let Some(command) = cli.command {
+        command.execute()?;
+    } else {
+        // No command provided. Show help.
+        Cli::command().print_long_help()?;
+    }
 
-    command.execute()
+    Ok(())
 }
 
 /// Handle the `run` command.
