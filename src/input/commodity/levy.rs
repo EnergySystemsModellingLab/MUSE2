5ae6d508dd918be682d442ee24901dfde7df214d
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-10-29 11:00:48 +0000

    Revert "Ensure right sign for net levies."
    This reverts commit bcf1ae1a2be090bbbda7db82cbc666f63602f5ba.

--- src/input/commodity/levy.rs
+++ src/input/commodity/levy.rs
@@ -130,7 +130,7 @@ where
                             try_insert(
                                 map_p,
                                 &(region.clone(), *year, time_slice.clone()),
-                                MoneyPerFlow(cost.value.0.abs()),
+                                cost.value,
                             )?;
                             let map_c = map
                                 .entry(BalanceType::Consumption)
@@ -138,7 +138,7 @@ where
                             try_insert(
                                 map_c,
                                 &(region.clone(), *year, time_slice.clone()),
-                                MoneyPerFlow(-cost.value.0.abs()),
+                                MoneyPerFlow(-cost.value.0),
                             )?;
                         }
                     }
