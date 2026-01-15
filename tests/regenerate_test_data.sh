ab6d02c7b3f3bf15ca0a55bd54b27bdb1ec7f2ed
Alex Dewar <a.dewar@imperial.ac.uk>
2026-01-15 15:17:24 +0000

    regenerate_test_data.sh: Allow for regenerating data for patched examples too

--- tests/regenerate_test_data.sh
+++ tests/regenerate_test_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euf
+set -ef
 
 mydir=$(dirname "$0")
 cd "$mydir"
@@ -7,11 +7,27 @@ cd "$mydir"
 echo Building MUSE2
 cargo -q build
 
-if [[ $# -gt 0 ]]; then
+if [ "$1" = --patch ]; then
+    shift 1
+    patch_examples=$@
+elif [ $# -gt 0 ]; then
     examples=$@
 else
     examples=$(cargo -q run example list)
+    patch_examples=$(cargo -q run example list --patch)
 fi
+
+run_example() {
+    example=$1
+    debug=$2
+    shift 2  # allow for passing extra args
+
+    echo Generating data for example: $example
+
+    env MUSE2_LOG_LEVEL=error \
+        cargo -q run example run -o "data/$example" "$example" \
+            --overwrite --debug-model=$debug $@
+}
 
 for example in $examples; do
     # Skip the circularity example
@@ -19,15 +35,15 @@ for example in $examples; do
         continue
     fi
 
-    echo Generating data for example: $example
-
     # We only need debug files for the simple model
-    extra_args=--overwrite
-    if [ $example = simple ]; then
-        extra_args="$extra_args --debug-model"
-    else
-        extra_args="$extra_args --debug-model=false"
+    debug=false
+    if [ "$example" = simple ]; then
+        debug=true
     fi
 
-    env MUSE2_LOG_LEVEL=error cargo -q run example run $extra_args -o "data/$example" "$example"
+    run_example "$example" $debug
+done
+
+for example in $patch_examples; do
+    run_example "$example" false --patch
 done
