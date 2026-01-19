717c7a7a1516f62b8ab4b6f73f2a5bb10bcfb0bb
Tom Bland <tom_bland@hotmail.co.uk>
2026-01-19 14:08:26 +0000

    Use new approach for regression test

--- tests/regression_circularity.rs
+++
@@ -1,8 +0,0 @@
-//! A regression test for the `circularity` example
-mod regression;
-use regression::run_regression_test;
-
-#[test]
-fn regression_circularity() {
-    run_regression_test("circularity");
-}
