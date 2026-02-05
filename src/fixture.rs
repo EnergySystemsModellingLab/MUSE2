2053c6021089794b22ee8f72509a3ab4d8d44a3f
Aurash Karimi <a.karimi@imperial.ac.uk>
2026-02-05 09:07:53 +0000

    fix patches

--- src/fixture.rs
+++ src/fixture.rs
@@ -418,14 +418,14 @@ mod tests {
     #[test]
     fn patch_and_validate_simple_fail() {
         let patch = FilePatch::new("commodities.csv")
-            .with_deletion("RSHEAT,Residential heating,svd,daynight,NOUNIT");
+            .with_deletion("RSHEAT,Residential heating,svd,daynight,PJ");
         assert!(patch_and_validate_simple!(vec![patch]).is_err());
     }
 
     #[test]
     fn patch_and_run_simple_fail() {
         let patch = FilePatch::new("commodities.csv")
-            .with_deletion("RSHEAT,Residential heating,svd,daynight,NOUNIT");
+            .with_deletion("RSHEAT,Residential heating,svd,daynight,PJ");
         assert!(patch_and_run_simple!(vec![patch]).is_err());
     }
 }
