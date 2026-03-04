438aabc264eb36f5dcad286d23112f269e184a8b
Tom Bland <tom_bland@hotmail.co.uk>
2026-03-04 13:45:50 +0000

    Allow toml patching for regression tests

--- src/example/patches.rs
+++ src/example/patches.rs
@@ -1,36 +1,42 @@
-//! File patches to be used in integration tests.
+//! Patches to be used in integration tests.
 //!
 //! This is used to test small variations on existing example models.
 use crate::patch::FilePatch;
 use anyhow::{Context, Result};
 use std::{collections::BTreeMap, sync::LazyLock};
 
-/// A map of file patches, keyed by name
-type PatchMap = BTreeMap<&'static str, Vec<FilePatch>>;
+/// Map of patches keyed by name, with the file patches and and optional TOML patch
+type PatchMap = BTreeMap<&'static str, (Vec<FilePatch>, Option<String>)>;
 
-/// The file patches, keyed by name
+/// The patches, keyed by name
 static PATCHES: LazyLock<PatchMap> = LazyLock::new(get_all_patches);
 
 /// Get all patches
 fn get_all_patches() -> PatchMap {
     [
+        // The simple example with gas boiler process made divisible
         (
-            // The simple example with gas boiler process made divisible
             "simple_divisible",
-            vec![
-                FilePatch::new("processes.csv")
-                    .with_deletion("RGASBR,Gas boiler,all,RSHEAT,2020,2040,1.0,")
-                    .with_addition("RGASBR,Gas boiler,all,RSHEAT,2020,2040,1.0,1000"),
-            ],
+            (
+                vec![
+                    FilePatch::new("processes.csv")
+                        .with_deletion("RGASBR,Gas boiler,all,RSHEAT,2020,2040,1.0,")
+                        .with_addition("RGASBR,Gas boiler,all,RSHEAT,2020,2040,1.0,1000"),
+                ],
+                None,
+            ),
         ),
         // The simple example with objective type set to NPV for one agent
         (
             "simple_npv",
-            vec![
-                FilePatch::new("agent_objectives.csv")
-                    .with_deletion("A0_RES,all,lcox,,")
-                    .with_addition("A0_RES,all,npv,,"),
-            ],
+            (
+                vec![
+                    FilePatch::new("agent_objectives.csv")
+                        .with_deletion("A0_RES,all,lcox,,")
+                        .with_addition("A0_RES,all,npv,,"),
+                ],
+                None,
+            ),
         ),
         (
             // The simple example with electricity priced using marginal costs
@@ -67,8 +73,8 @@ pub fn get_patch_names() -> impl Iterator<Item = &'static str> {
 }
 
 /// Get patches for the named patched example
-pub fn get_patches(name: &str) -> Result<&[FilePatch]> {
-    Ok(PATCHES
+pub fn get_patches(name: &str) -> Result<&'static (Vec<FilePatch>, Option<String>)> {
+    PATCHES
         .get(name)
-        .with_context(|| format!("Patched example '{name}' not found"))?)
+        .with_context(|| format!("Patched example '{name}' not found"))
 }
