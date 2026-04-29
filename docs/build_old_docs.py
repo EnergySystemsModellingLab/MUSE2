584eaaeb1026c7b1310a3345ab66022854b587bd
Alex Dewar <a.dewar@imperial.ac.uk>
2026-04-29 16:42:22 +0100

    build_old_docs.py: Use different git worktrees for each release

--- docs/build_old_docs.py
+++ docs/build_old_docs.py
@@ -16,19 +16,43 @@ DOCS_DIR = REPO_ROOT / "docs"
 
 
 def clone_repo_to(dest: Path):
-    """Clone this repo somewhere else."""
+    """Make a bare clone of this repo somewhere else."""
     print(f"Making a copy of repo in {dest}")
-    sp.run(("git", "clone", REPO_ROOT, dest), check=True, capture_output=True)
+    sp.run(("git", "clone", "--bare", REPO_ROOT, dest), check=True, capture_output=True)
+
+
+def add_worktree_for_release(repo_path: Path, release: str) -> Path:
+    """Add a worktree for the current release.
+
+    Also tries to symlink to the cargo cache dir to speed up build times, if possible.
+    """
+    # Add new worktree
+    release_path = repo_path.parent / release
+    sp.run(
+        (
+            "git",
+            "-C",
+            str(repo_path),
+            "worktree",
+            "add",
+            release_path,
+            release,
+        ),
+        check=True,
+        capture_output=True,
+    )
 
     # Add a symlink to cargo cache dir
     try:
-        os.symlink(REPO_ROOT / "target", dest / "target")
+        os.symlink(REPO_ROOT / "target", release_path / "target")
     except (NotImplementedError, OSError):
         # Only newer versions of Windows support symlinks and these require the user to have
         # additional privileges (or to be in developer mode)
         print(
             "WARN: Could not create symlink to cache directory; cache will not be stored"
         )
+
+    return release_path
 
 
 def apply_patches_for_release(release: str, repo_path: Path) -> None:
@@ -41,22 +65,16 @@ def apply_patches_for_release(release: str, repo_path: Path) -> None:
 def build_docs_for_release(release: str, repo_path: Path, outdir: Path) -> None:
     """Build documentation for a given release."""
     print(f"Building docs for {release}")
-
-    # Check out release
-    sp.run(
-        ("git", "-C", str(repo_path), "checkout", release),
-        check=True,
-        capture_output=True,
-    )
+    release_path = add_worktree_for_release(repo_path, release)
 
     # Apply patches, if any
-    apply_patches_for_release(release, repo_path)
+    apply_patches_for_release(release, release_path)
 
     # Build docs
-    sp.run(("just", f"{repo_path!s}/build-docs"), check=True)
+    sp.run(("just", f"{release_path!s}/build-docs"), check=True)
 
     # Patch versions.html to redirect to main versions page
-    with (repo_path / "book" / "versions.html").open("w", encoding="utf-8") as f:
+    with (release_path / "book" / "versions.html").open("w", encoding="utf-8") as f:
         f.write(f"""<head>
     <meta http-equiv="Refresh" content="0; URL={DOCS_SITE_ROOT}/versions.html" />
 </head>""")
@@ -64,7 +82,7 @@ def build_docs_for_release(release: str, repo_path: Path, outdir: Path) -> None:
     # Move to output directory
     release_outdir = outdir / release
     print(f"Copying to {release_outdir}")
-    shutil.move((repo_path / "book"), release_outdir)
+    shutil.move((release_path / "book"), release_outdir)
 
 
 def build_old_docs() -> None:
@@ -74,7 +92,7 @@ def build_old_docs() -> None:
 
     # Clone this repo to a temporary directory
     with TemporaryDirectory() as tmpdir:
-        repo_path = Path(tmpdir)
+        repo_path = Path(tmpdir) / "MUSE2.git"
         clone_repo_to(repo_path)
 
         # Generate documentation for each previous release
