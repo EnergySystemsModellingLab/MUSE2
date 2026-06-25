25092b68062a96ba4a9a24b4aa2151ecf23d79b2
Dan Cummins <daniel.cummins17@imperial.ac.uk>
2026-06-25 17:16:02 +0100

    Replace 'investment set' -> 'market set'

--- src/simulation/prices.rs
+++ src/simulation/prices.rs
@@ -96,10 +96,10 @@ pub fn calculate_prices(
     // Lazily computed only if at least one FullCost market is encountered.
     let mut annual_activities: Option<HashMap<AssetRef, Activity>> = None;
 
-    // Iterate over investment sets in reverse order
-    for investment_set in model.investment_order[&year].iter().rev() {
-        price_investment_set(
-            investment_set,
+    // Iterate over market sets in reverse order
+    for market_set in model.investment_order[&year].iter().rev() {
+        price_market_set(
+            market_set,
             model,
             solution_without_candidates,
             solution_with_candidates,
@@ -116,10 +116,10 @@ pub fn calculate_prices(
     })
 }
 
-/// Calculate prices for the markets in an investment set, updating `market_prices`.
+/// Calculate prices for the markets in an market set, updating `market_prices`.
 #[allow(clippy::too_many_arguments)]
-fn price_investment_set(
-    investment_set: &MarketSet,
+fn price_market_set(
+    market_set: &MarketSet,
     model: &Model,
     solution_without_candidates: &Solution,
     solution_with_candidates: &Solution,
@@ -128,7 +128,7 @@ fn price_investment_set(
     annual_activities: &mut Option<HashMap<AssetRef, Activity>>,
     market_prices: &mut PriceMap,
 ) {
-    match investment_set {
+    match market_set {
         MarketSet::Single(market) => {
             price_markets(
                 model,
@@ -153,9 +153,9 @@ fn price_investment_set(
                 market_prices,
             );
         }
-        MarketSet::Layer(investment_sets) => {
-            for set in investment_sets {
-                price_investment_set(
+        MarketSet::Layer(market_sets) => {
+            for set in market_sets {
+                price_market_set(
                     set,
                     model,
                     solution_without_candidates,
