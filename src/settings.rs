755d0f418a6bf9434744034c63d04cf48b23574e
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2026-07-01 17:17:24 +0100

    Link to latest version of docs in api documentation text.

--- src/settings.rs
+++ src/settings.rs
@@ -23,7 +23,9 @@ const DEFAULT_SETTINGS_FILE_HEADER: &str = concat!(
 # \tmuse2 settings show-default
 #
 # For information about the possible settings, visit:
-# \thttps://energysystemsmodellinglab.github.io/MUSE2/file_formats/program_settings.html
+# \thttps://energysystemsmodellinglab.github.io/MUSE2/v",
+    env!("CARGO_PKG_VERSION"),
+    "/file_formats/program_settings.html
 "
 );
 
