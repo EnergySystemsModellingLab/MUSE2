75a05f3c5a071f6e2464330c37662398b3d40a1b
Diego Alonso Álvarez <6095790+dalonsoa@users.noreply.github.com>
2026-07-10 11:58:15 +0100

    Change error handling to panic for unmet demand writer

--- src/output.rs
+++ src/output.rs
@@ -449,10 +449,9 @@ impl DebugDataWriter {
         }
 
         // If the unmet demand writer already exist, we return with an error, as it should not happen
-        ensure!(
-            self.unmet_demand_writer.is_none(),
-            "Unmet demand file already exists!"
-        );
+        if self.unmet_demand_writer.is_some() {
+            panic!("Unmet demand file already exists!");
+        }
 
         self.unmet_demand_writer = Some(csv::Writer::from_path(&self.unmet_demand_file_path)?);
         for (commodity_id, region_id, time_slice, value) in rows {
