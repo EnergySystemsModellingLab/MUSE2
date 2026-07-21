b6bf93a6405beb0de54dcdb93b5533e807636983
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2026-07-21 14:09:43 +0100

    Refactor timing contexts

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,7 +4,7 @@ use crate::model::Model;
 use crate::output::DataWriter;
 use crate::process::ProcessMap;
 use crate::simulation::prices::{Prices, calculate_prices};
-use crate::timeit::TimingContext;
+use crate::timeit::DispatchContext;
 use crate::units::Capacity;
 use anyhow::{Context, Result};
 use context_manager;
@@ -160,7 +160,7 @@ pub fn run(model: &Model, output_path: &Path, debug_model: bool) -> Result<()> {
 }
 
 // Run dispatch to get flows and prices for a milestone year
-#[context_manager::wrap(TimingContext)]
+#[context_manager::wrap(DispatchContext)]
 fn run_dispatch_for_year(
     model: &Model,
     assets: &[AssetRef],
--- src/timeit.rs
+++ src/timeit.rs
@@ -1,26 +1,64 @@
 //! Implement a context manager for timing the decorated function
 use context_manager::{CallerContext, SyncWrapContext};
-use log::info;
-use std::time::Instant;
+use log::debug;
+use std::{marker::PhantomData, time::Instant};
 
-/// Context for time a function call
-pub struct TimingContext {
-    start: Instant,
+#[doc(hidden)]
+pub trait TimerLabel {
+    fn label(caller_context: &CallerContext) -> String;
 }
 
-impl<T> SyncWrapContext<T> for TimingContext {
+/// Generic timing implementation shared by all timing contexts
+pub struct TimerContext<L: TimerLabel> {
+    start: Instant,
+    _label: PhantomData<L>,
+}
+
+impl<T, L: TimerLabel> SyncWrapContext<T> for TimerContext<L> {
     fn new() -> Self {
         Self {
             start: Instant::now(),
+            _label: PhantomData,
         }
     }
 
     fn after(self, caller_context: &CallerContext, _result: &T) {
-        let elapsed = self.start.elapsed();
-        info!(
-            "[Timer] '{}' completed in {:.3}ms",
-            caller_context.fn_name(),
-            elapsed.as_secs_f64() * 1000.0,
-        );
+        let label = L::label(caller_context);
+        let ms = self.start.elapsed().as_secs_f64() * 1000.0;
+        debug!("{label} completed in {ms:.1}ms");
     }
 }
+
+#[doc(hidden)]
+pub struct FnNameLabel;
+impl TimerLabel for FnNameLabel {
+    fn label(caller_context: &CallerContext) -> String {
+        let fn_name = caller_context.fn_name();
+        format!("[Timer] '{fn_name}'")
+    }
+}
+
+#[doc(hidden)]
+pub struct InvestmentLabel;
+impl TimerLabel for InvestmentLabel {
+    fn label(_: &CallerContext) -> String {
+        "Investment step".to_string()
+    }
+}
+
+#[doc(hidden)]
+pub struct DispatchLabel;
+impl TimerLabel for DispatchLabel {
+    fn label(_: &CallerContext) -> String {
+        "Dispatch step".to_string()
+    }
+}
+
+/// Generic context for timing a function call
+pub type TimingContext = TimerContext<FnNameLabel>;
+
+/// Investment context for timing a function call
+pub type InvestmentContext = TimerContext<InvestmentLabel>;
+
+/// Dispatch context for timing a function call
+pub type DispatchContext = TimerContext<DispatchLabel>;
