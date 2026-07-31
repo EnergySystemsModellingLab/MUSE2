207e2850c0693ba759bd14515bf7c2f94849d8ce
Alex Dewar <a.dewar@imperial.ac.uk>
2026-07-31 17:27:01 +0100

    Remove stale comment

--- src/simulation/market.rs
+++ src/simulation/market.rs
@@ -405,9 +405,7 @@ fn get_asset_options<'a>(
     year: u32,
     capacity_limit_factor: Dimensionless,
 ) -> impl Iterator<Item = AssetRef> + 'a {
-    // Get existing assets which produce the commodity of interest. We discard information about
-    // mothballed units here; it is preserved in the original collection of assets and is fed back
-    // into `AssetPool` later.
+    // Get existing assets which produce the commodity of interest
     let existing_assets = all_existing_assets
         .iter()
         .filter_agent(&agent.id)
