06d87e3677ee5b2344edc7bcafa1ab2956278d09
Tom Bland <tom_bland@hotmail.co.uk>
2026-08-13 16:44:14 +0100

    Smarter asset grouping

--- src/simulation/optimisation/constraints.rs
+++ src/simulation/optimisation/constraints.rs
@@ -3,7 +3,6 @@ use super::VariableMap;
 use crate::asset::{AssetCapacity, AssetIterator, AssetRef};
 use crate::commodity::{CommodityID, CommodityType};
 use crate::model::Model;
-use crate::process::ProcessID;
 use crate::region::RegionID;
 use crate::time_slice::{Season, TimeSliceInfo, TimeSliceSelection};
 use crate::units::{Flow, MoneyPerCapacityPerYear, UnitType, Year};
@@ -474,8 +473,8 @@ where
     ActivityKeys { offset, keys }
 }
 
-/// Add constraints requiring assets of the same process in the same region to have equal
-/// utilisation in each time slice.
+/// Add constraints requiring dispatch-equivalent assets to have equal utilisation in each time
+/// slice.
 ///
 /// Flexible-capacity assets are excluded because their maximum activity depends on a decision
 /// variable. The constraints added here are not included in [`ConstraintKeys`], as their duals
@@ -494,19 +493,23 @@ fn add_equal_utilisation_constraints<'a, I>(
         .map(|(asset, _)| asset)
         .collect();
 
-    // Group together assets with the same process and region
-    let mut assets_by_process: IndexMap<(RegionID, ProcessID), Vec<&AssetRef>> = IndexMap::new();
+    // Group assets by comparing each one with the first asset in each group.
+    let mut asset_groups: Vec<Vec<&AssetRef>> = Vec::new();
     for asset in assets.filter(|asset| !flexible_assets.contains(asset)) {
-        assets_by_process
-            .entry((asset.region_id().clone(), asset.process_id().clone()))
-            .or_default()
-            .push(asset);
+        if let Some(group) = asset_groups
+            .iter_mut()
+            .find(|group| group[0].is_dispatch_equivalent(asset))
+        {
+            group.push(asset);
+        } else {
+            asset_groups.push(vec![asset]);
+        }
     }
 
     // For each group of assets, add constraints to force equal utilisation in each time slice
     // This is done by anchoring each asset to the first asset in the group (-> (n-1) constraints
     // for a group of n assets)
-    for assets in assets_by_process.into_values() {
+    for assets in asset_groups {
         let Some((reference_asset, others)) = assets.split_first() else {
             continue;
         };
