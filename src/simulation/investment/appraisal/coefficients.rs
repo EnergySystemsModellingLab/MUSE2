1413b6eaa9b0c5e35561c6ebbf820c0a442d6b9e
Tom Bland <tom_bland@hotmail.co.uk>
2026-09-14 16:07:03 +0100

    EXPERIMENT: EPSILON_ACTIVITY_COEFFICIENT = 0

--- src/simulation/investment/appraisal/coefficients.rs
+++ src/simulation/investment/appraisal/coefficients.rs
@@ -70,7 +70,7 @@ pub fn calculate_coefficients_for_asset(
     // Small constant added to each activity coefficient to ensure break-even/slightly negative
     // assets are still dispatched
     // const EPSILON_ACTIVITY_COEFFICIENT: MoneyPerActivity = MoneyPerActivity(f64::EPSILON * 100.0);
-    const EPSILON_ACTIVITY_COEFFICIENT: MoneyPerActivity = MoneyPerActivity(1e-4);
+    const EPSILON_ACTIVITY_COEFFICIENT: MoneyPerActivity = MoneyPerActivity(0.0);
 
     // Activity coefficients
     let mut activity_coefficients = IndexMap::new();
